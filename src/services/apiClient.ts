adee3c9a06b561f0c7801adb64541e3ec6897922
elkhvn <elxankhabib@gmail.com>
2024-06-19 09:25:11 +0200

    Fetch the games

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-axios.create({
+export default axios.create({
     baseURL: 'https://api.rawg.io/api',
     params: {
         key: 'ef4b1ae0362d4600a999bfce55475c46'

ba14889a7aaef696672ad331f30b82dd51847dfa
elkhvn <elxankhabib@gmail.com>
2024-06-28 13:00:45 +0200

    Add emojis

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -13,7 +13,8 @@ export interface Game {
     background_image: string;
     parent_platforms: {platform: Platform}[];
     metacritic: number;
-  }
+    rating_top: number;
+}
 
 const useGames = (gameQuery: GameQuery )=> {
     return useData<Game>('/games', {
